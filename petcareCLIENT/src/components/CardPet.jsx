0ff7526cb4fc1d438fb771940437a7839c34d3bf
palcojulian <palcojulian@outlook.com>
2024-11-30 21:33:23 -0500

    Implementa accion de editar la mascota

--- petcareCLIENT/src/components/CardPet.jsx
+++ petcareCLIENT/src/components/CardPet.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CardPet.css'; // Importa los estilos
 
-const TarjetasMascotas = ({ data }) => {
-  return (
-    <div className="scroll-container">
-      <div className="grid-container">
-        {data.map((mascota) => (
-          <div key={mascota.id} className="card">
-            <h3 className="card-title">{mascota.nombre}</h3>
-            <p><strong>Especie:</strong> {mascota.especie}</p>
-            <p><strong>Raza:</strong> {mascota.raza}</p>
-            <p><strong>Edad:</strong> {mascota.edad} año(s)</p>
-            <p><strong>Descripción:</strong> {mascota.descripcion}</p>
-            <p>
-              <strong>Estado de adopción:</strong>{' '}
-              <span className={mascota.estado_adopcion ? 'adopted' : 'available'}>
-                {mascota.estado_adopcion ? 'Adoptado' : 'Disponible'}
-              </span>
-            </p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+const TarjetasMascotas = ({ data, onEdit }) => {
+    return (
+        <div className="scroll-container">
+            <div className="grid-container">
+                {data.map((mascota) => (
+                    <div key={mascota.id} className="card">
+                        <h3 className="card-title">
+                            {mascota.nombre}
+                        </h3>
+                        <p><b>Especie:</b> {mascota.especie}</p>
+                        <p><b>Raza:</b> {mascota.raza}</p>
+                        <p><b>Edad:</b> {mascota.edad} año(s)</p>
+                        <p><b>Descripción:</b> {mascota.descripcion}</p>
+                        <p>
+                            <b>Estado de adopción:</b>{' '}
+                            <span className={mascota.estado_adopcion ? 'adopted' : 'available'}>
+                                {mascota.estado_adopcion ? 'Adoptado' : 'Disponible'}
+                            </span>
+                        </p>
+
+                        <button className="edit-button" onClick={() => onEdit(mascota)}>
+                            Editar
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default TarjetasMascotas;
--- petcareCLIENT/src/components/EditPet.jsx
+++ petcareCLIENT/src/components/EditPet.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { getPets } from './../helpers/getPets'
 import CardPet from './CardPet'
+import FormEditar from './FormEditar'
+import Modal from './Modal'
 
 const EditPet = () => {
     const [pets, setPets] = useState([]);
+    const [selectedMascota, setSelectedMascota] = useState(null);
+    const [isModalPetSelected, setModalPetSelected] = useState(false);
+
+
+    const handleEdit = (mascota) => {
+        setModalPetSelected(true);
+
+        setSelectedMascota(mascota); // Actualiza la mascota seleccionada
+        console.log('Mascota seleccionada para editar:', mascota); // Acción o lógica adicional
+    };
+
 
     const consult = async () => {
         try {
@@ -14,14 +27,23 @@ const EditPet = () => {
         }
     }
 
+    const closeModal = () => {
+        setModalPetSelected(false);
+    }
+
+
     useEffect(() => {
-       consult();
+        consult();
     }, []);
 
 
     return (
         <div >
-            <CardPet data={pets} />
+            <CardPet data={pets} onEdit={handleEdit} />
+
+            <Modal titleModal={"MASCOTA SELECCIONADA"} isOpen={isModalPetSelected} onClose={closeModal}>
+                <FormEditar mascota={selectedMascota} />
+            </Modal>
         </div>
     );
 };
